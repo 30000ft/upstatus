9d2d753741fc56b15235c5b1b4d20351de49da2d
30000ft <188370985@qq.com>
2023-04-04 19:21:49 +0800

    bug fix

--- server.py
+++ server.py
@@ -10,13 +10,14 @@ load_dotenv()  # 加载环境变量
 
 # 读取环境变量
 server_port = int(os.getenv("SERVER_PORT"))
-timeout = int(os.getenv("TIMEOUT"))
+heartbeat_interval = int(os.getenv("HEARTBEAT_INTERVAL"))
+heartbeat_timeout = int(os.getenv("HEARTBEAT_TIMEOUT"))
 email_sender = os.getenv("EMAIL_ADDRESS")
 email_password = os.getenv("EMAIL_PASSWORD")
 email_recipient = os.getenv("EMAIL_RECIPIENT")
 smtpemail_host = os.getenv("SMTPEMAIL_HOST")
 
-clients = {}  # 存储客户端的IP地址和状态
+clients_connected = {}  # 存储客户端的IP地址和状态
 
 def send_email_notification(subject, content):
     msg = EmailMessage()
@@ -32,11 +33,11 @@ def send_email_notification(subject, content):
     server.quit()
 
 def handle_client(conn, addr):
-    global clients
-    conn.settimeout(timeout)
+    global clients_connected
+    conn.settimeout(heartbeat_timeout)
     try:
         ip = addr[0]
-        clients[ip] = False
+        clients_connected[ip] = False
 
         while True:
             data = conn.recv(1024)
@@ -45,19 +46,21 @@ def handle_client(conn, addr):
 
             if data.decode() == "heartbeat":
                 print(f"来自 {ip} 的心跳消息。")
-                if clients[ip]:
-                    clients[ip] = False
+                if not clients_connected[ip]:
+                    clients_connected[ip] = True
                     send_email_notification(f"{ip}网络恢复通知", f"{ip}内网与互联网重新连接了。")
                 else:
-                    clients[ip] = True
+                    print(f"来自 {ip} 的重复心跳消息。")
                 conn.send("ok".encode())
             else:
                 print(f"来自 {ip} 的无效心跳消息。")
 
     except socket.timeout:
-        if not clients[ip]:
-            clients[ip] = True
+        if clients_connected[ip]:
+            clients_connected[ip] = False
             send_email_notification(f"{ip}网络中断通知", f"{ip}内网与互联网断开连接了。")
+        else:
+            print(f"来自 {ip} 的重复超时消息。")
     finally:
         conn.close()
 
